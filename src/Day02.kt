98ec3a2eb4a0f24e7bc995829073051e6c0dd305
geist-2501 <blaironbute@gmail.com>
2021-12-16 20:20:49 +0000

    Completed day 2

--- src/Day02.kt
+++ src/Day02.kt
@@ -1,3 +1,43 @@
 fun main() {
+    fun part1(instructions: List<String>): Int {
+        var x = 0
+        var y = 0
 
+        for (inst in instructions) {
+            val (comm, value) = inst.split(' ')
+            val v = value.toInt()
+            when(comm) {
+                "forward" -> x += v
+                "down" -> y += v
+                "up" -> y -= v
+            }
+        }
+
+        return x * y
+    }
+
+    fun part2(instructions: List<String>): Int {
+        var x = 0
+        var y = 0
+        var aim = 0
+
+        for (inst in instructions) {
+            val (comm, value) = inst.split(' ')
+            val v = value.toInt()
+            when(comm) {
+                "forward" -> {
+                    x += v
+                    y += aim * v
+                }
+                "down" -> aim += v
+                "up" -> aim -= v
+            }
+        }
+
+        return x * y
+    }
+
+    val input = readInput("resources/day02.txt")
+    println(part1(input))
+    println(part2(input))
 }
